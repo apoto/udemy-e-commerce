1e2e0c644c29e358ca4381adc01379dc90f98170
apoto <apopo.apoto930@gmail.com>
2023-12-24 16:12:31 +0900

    Enable font-awesome.js

--- src/plugins/BackTheme/templates/layout/default.php
+++ src/plugins/BackTheme/templates/layout/default.php
@@ -14,7 +14,6 @@
   <!-- CSS -->
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <?= $this->Html->css([
-    'BackTheme./css/fontawesome.min',
     'BackTheme./css/sb-admin-2.min',
   ]); ?>
 </head>
@@ -40,6 +39,7 @@
 
 	<!-- Scripts -->
 	<?= $this->Html->script([
+		'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js',
 		'BackTheme./js/jquery.min',
 		'BackTheme./js/bootstrap.bundle.min',
 		'BackTheme./js/jquery.easing.min.min',
