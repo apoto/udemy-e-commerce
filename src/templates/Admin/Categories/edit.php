a8af6e30c3a388dd57e2f9293052a46f00d8163b
apoto <apopo.apoto930@gmail.com>
2023-12-29 21:10:10 +0900

    Create category editing function.

--- src/templates/Admin/Categories/edit.php
+++ src/templates/Admin/Categories/edit.php
@@ -11,14 +11,14 @@
     <div class="card shadow my-4">
     <?= $this->Form->create($category) ?> 
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">カテゴリー追加</h6>
+            <h6 class="m-0 font-weight-bold text-primary">カテゴリー編集</h6>
         </div>
         <div class="card-body">
             <?= $this->Form->control('name', ['class' => 'form-control']) ?>
 
         </div>
         <div class="card-footer">
-        <?= $this->Form->submit('追加', ['class' => 'btn btn-success']) ?>
+        <?= $this->Form->submit('保存', ['class' => 'btn btn-success']) ?>
         
         </div>
     <?= $this->Form->end() ?> 
