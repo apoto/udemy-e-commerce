c654b07628fbaf899e562347d2d69596c1426802
apoto <apopo.apoto930@gmail.com>
2024-01-04 22:53:39 +0900

    Create edit photo function

--- src/templates/Admin/Photos/edit.php
+++ src/templates/Admin/Photos/edit.php
@@ -1,7 +1,7 @@
 <div class="container-fluid mt-5">
     <div class="row">
         <div class="col">
-            <h1 class="h3 mb-2 text-gray-800">Option the characteristic</h1>
+            <h1 class="h3 mb-2 text-gray-800">Photo</h1>
         </div>
     </div>
     
@@ -9,12 +9,19 @@
 
     <!-- DataTales Example -->
     <div class="card shadow my-4">
-    <?= $this->Form->create($characteristicValue) ?> 
+    <?= $this->Form->create($photo, ['type' => 'file']) ?> 
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">特徴オプション編集</h6>
+            <h6 class="m-0 font-weight-bold text-primary">写真編集</h6>
         </div>
         <div class="card-body">
-            <?= $this->Form->control('name', ['class' => 'form-control']) ?>
+            <div class="row">
+                <div class="col-md-6">
+                    <?= $this->Form->control('alt', ['class' => 'form-control']) ?>
+                </div>
+                <div class="col-md-6">
+                    <?= $this->Form->control('file_name', ['class' => 'form-control', 'type' => 'file']) ?>
+                </div>
+            </div>
 
         </div>
         <div class="card-footer">
