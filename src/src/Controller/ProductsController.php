8953032be21bb7476e72488bd99a0377883cb294
apoto <apopo.apoto930@gmail.com>
2024-01-10 21:59:36 +0900

    add: Product list screen.

---
+++ src/src/Controller/ProductsController.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Products Controller
+ *
+ * @property \App\Model\Table\ProductsTable $Products
+ * @method \App\Model\Entity\Product[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ProductsController extends AppController
+{
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);
+
+        $this->Authentication->addUnauthenticatedActions(['index']);
+    }
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index($categoryId)
+    {
+        $products = $this->Products->find()->where(['category_id' => $categoryId]);
+
+        $this->set(['products' => $this->paginate($products)]);
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Product id.
+     * @return \Cake\Http\Response|null|void Renders view
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $product = $this->Products->get($id, [
+            'contain' => ['Categories', 'CharacteristicValues', 'Photos', 'OrderLines'],
+        ]);
+
+        $this->set(compact('product'));
+    }
+}
